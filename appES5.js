f3fd4004a0adc21c7af9a1099faaa0939b52bb5c
ismailonur <ismailonur15@gmail.com>
2020-12-27 23:36:49 +0300

    eleman silme işlemi yapıldı

--- appES5.js
+++ appES5.js
@@ -16,7 +16,7 @@ UI.prototype.addCourseToList = function (course) {
             <td><img src="img/${course.image}"/></td>
             <td>${course.title}</td>
             <td>${course.instructor}</td>
-            <td><a href="#" class="btn btn-danger btn-sm">Delete</a></td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
          </tr>    
     `;
 
@@ -27,6 +27,12 @@ UI.prototype.clearControls = function () {
     const title = document.getElementById('title').value = "";
     const instructor = document.getElementById('instructor').value = "";
     const image = document.getElementById('image').value = "";
+}
+
+UI.prototype.deleteCourse = function(element){
+    if(element.classList.contains('delete')){
+        element.parentElement.parentElement.remove();
+    }
 }
 
 
@@ -46,4 +52,10 @@ document.getElementById('new-course').addEventListener('submit', function (e) {
 
 
     e.preventDefault();
+})
+
+document.getElementById('course-list').addEventListener('click', function(e){
+    const ui = new UI();
+    ui.deleteCourse(e.target);
+
 })
