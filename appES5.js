0e8b51ba8a29fe638aebcaab0b6abe9ad22aa5b6
ismailonur <ismailonur15@gmail.com>
2020-12-27 23:11:53 +0300

    appES5'te Course objesi oluşturuldu

---
+++ appES5.js
@@ -0,0 +1,19 @@
+function Course(title, instructor, image){
+    this.title = title;
+    this.instructor = instructor;
+    this.image = image;
+}
+
+
+document.getElementById('new-course').addEventListener('submit', function(e){
+    
+    const title = document.getElementById('title').value;
+    const instructor = document.getElementById('instructor').value;
+    const image = document.getElementById('image').value;
+    
+    const course = new Course(title, instructor, image);
+    console.log(course);
+    
+    
+    e.preventDefault();    
+})
